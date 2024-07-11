67a98184c1bc116934cb5935d49bd68f09e553fb
AnandPTi <anandpr@iitbhilai.ac.in>
2024-07-11 19:29:52 +0530

    changed name

--- booking/app/page.tsx
+++ booking/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl underline">Homees</h1>
+      <h1 className="text-3xl underline">Homwantees</h1>
     </div>
   )
 }
