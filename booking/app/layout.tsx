7141d0cd8b971eca18c845818107ca6a596fee06
AnandPTi <anandpr@iitbhilai.ac.in>
2024-07-11 18:51:30 +0530

    1st try

---
+++ booking/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Plus_Jakarta_Sans } from "next/font/google";
+import "./globals.css";
+
+import { cn } from '@/lib/utils'
+
+
+const fontSans = Plus_Jakarta_Sans({ 
+  subsets: ["latin"],
+  weight: ['300', '400', '500', '600', '700'],
+  variable: '--font-sans'
+ });
+
+export const metadata: Metadata = {
+  title: "CarePulse",
+  description: "A Healthcare Management System",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={cn('min-h-screen bg-dark-300 font-sans antialiased', fontSans.variable)}>{children}</body>
+    </html>
+  );
+}
