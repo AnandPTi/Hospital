7129ddfcac3ee563228c5d464f01938ab2a7e876
AnandPTi <anandpr@iitbhilai.ac.in>
2024-07-23 14:47:08 +0530

    checkbox for privacy and disclosures

--- booking/components/CustomFormField.tsx
+++ booking/components/CustomFormField.tsx
@@ -21,6 +21,7 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { Select, SelectValue, SelectTrigger, SelectContent} from './ui/select';
 import { Textarea } from './ui/textarea';
+import { Checkbox } from './ui/checkbox';
 
 interface CustomProps{
     control: Control<any>,
@@ -126,6 +127,19 @@ const RenderField = ({field, props}: {field: any; props: CustomProps}) =>{
         case FormFieldType.SKELETON:
             return renderSkeleton ? renderSkeleton
             (field) : null
+        case FormFieldType.CHECKBOX:
+            return (<FormControl>
+                <div className="flex items-center gap-4">
+                    <Checkbox 
+                        id={props.name}
+                        checked={field.value}
+                        onCheckedChange={field.onChange}
+                    />
+                    <label htmlFor={props.name} className="checkbox-label">
+                        {props.label}
+                    </label>
+                </div>
+            </FormControl>)
         default:
             break;
     }
--- booking/components/forms/RegisterForm.tsx
+++ booking/components/forms/RegisterForm.tsx
@@ -276,7 +276,31 @@ const RegisterForm = ({user}: {user: User}) => {
                       <FileUploader files={field.value} onChange={field.onChange} />
                     </FormControl>
                 )}
-            />
+        />
+        <section className="space-y-6">
+            <div className="mb-9 space-y-1">
+                <h2 className="sub-header">Consent and Privacy</h2>
+            </div>
+        </section>
+        <CustomFormField
+          fieldType={FormFieldType.CHECKBOX}
+          control={form.control}
+          name="treatmentConsent"
+          label="I consent to treatment"
+        />
+        <CustomFormField
+          fieldType={FormFieldType.CHECKBOX}
+          control={form.control}
+          name="disclosureConsent"
+          label="I consent to disclosure of information"
+        />
+        <CustomFormField
+          fieldType={FormFieldType.CHECKBOX}
+          control={form.control}
+          name="privacyConsent"
+          label="I consent to privacy policy"
+        />
+
         <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
       </form>
     </Form>
